3267698f0fdc156fb725cafb55877da2df589f4c
160020020 <160020020@iitdh.ac.in>
2021-03-01 23:43:12 +0530

    Final

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Assignment 1 SIL801
-Prediction a 10 digit number from dataset of handwritten digits
+### Prediction a 10 digit number from dataset of handwritten digits
+In this assignment, we are supposed to identify a mobile no from a filled form. To do this, we first improve upon LeNet to build a single digit classifier and then use RNN on top of that to identify the full mobile number given the information that the mobile number is written by the same person.
 
 ## How to run the code?
 #### Single Digit Classification
@@ -9,5 +10,17 @@ Prediction a 10 digit number from dataset of handwritten digits
 #### Full Sequence Class
 <code> python sequential.py </code>
 
-
+###References
+1. https://medium.com/analytics-vidhya/improving-accuracy-using-deep-learning-for-fashion-mnist-d7a3516af347
+2. https://towardsdatascience.com/going-beyond-99-mnist-handwritten-digits-recognition-cfff96337392
+3. https://github.com/PAN001/MNIST-digit-sequence-recognition
+4. https://www.diva-portal.org/smash/get/diva2:1293077/FULLTEXT02.pdf
+5. http://petr-marek.com/blog/2017/07/05/sequence-digits-recognition/
+6. https://medium.com/swlh/multi-digit-sequence-recognition-with-crnn-and-ctc-loss-using-pytorch-framework-269a7aca2a6)
+7. [https://medium.com/swlh/multi-digit-sequence-recognition-with-crnn-and-ctc-loss-using-pytorch-framework-269a7aca2a6](https://medium.com/swlh/multi-digit-sequence-recognition-with-crnn-and-ctc-loss-using-pytorch-framework-269a7aca2a6)
+8. https://sajalsharma.com/portfolio/digit_sequence_recognition
+9. https://sajalsharma.com/portfolio/digit_sequence_recognition
+10. https://sajalsharma.com/portfolio/digit_sequence_recognition
+11. [https://papers.nips.cc/paper/6637-gated-recurrent-convolution-neural-network-for-ocr.pdf](https://papers.nips.cc/paper/6637-gated-recurrent-convolution-neural-network-for-ocr.pdf)
+12. [https://arxiv.org/abs/1507.05717](https://arxiv.org/abs/1507.05717)
 
--- sequential.py
+++ sequential.py
@@ -12,7 +12,7 @@ from tqdm import tqdm
 from colorama import Fore
 import sys
 from itertools import groupby
-from CRNN import CRNN
+from torch.autograd import Variable
 
 digits_per_sequence = 10 
 gpu = torch.device('cuda')
@@ -24,7 +24,7 @@ transformed_random_digits_images = []
 src_root = './assignment-1-dataset/dataset'
 save_root = './dataset_sequence'
 
-text = open('./assignment-1-dataset/groundTruthValues.txt')
+text = open('groundTruthValues.txt')
 lines = text.readlines()
 
 for linenumber, line in enumerate(lines[1:]):
@@ -82,7 +82,53 @@ gru_hidden_size = 64
 num_classes = 12
 blank_label = 11
 
-model = CRNN(cnn_output_height, gru_hidden_size, gru_num_layers, num_classes).to(gpu)
+class CRNN(nn.Module):
+    
+    def __init__(self):
+        super(CRNN, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=(3, 3)),
+            nn.InstanceNorm2d(32),
+            nn.ReLU(),
+        )
+        
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(32, 32, kernel_size=(3, 3)),
+            nn.InstanceNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=(3, 3)),
+            nn.InstanceNorm2d(64),
+            nn.ReLU(),
+        )
+    
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=(3, 3)),
+            nn.InstanceNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.gru_input_size = cnn_output_height * 64
+        self.gru = nn.GRU(self.gru_input_size, gru_hidden_size, gru_num_layers, batch_first=True, bidirectional=True)
+        self.fc = nn.Linear(gru_hidden_size * 2, num_classes)
+
+    def forward(self, x):
+        batch_size = x.shape[0]
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = out.permute(0, 3, 2, 1)        
+        out = out.reshape(batch_size, -1, self.gru_input_size)
+        out, _ = self.gru(out)
+        out = torch.stack([F.log_softmax(self.fc(out[i]), dim=-1) for i in range(out.shape[0])])
+        return out
+
+# model = LeNetGRU().to(gpu)
+# model = LeNet(11).to(gpu)
+model = CRNN().to(gpu)
 criterion = nn.CTCLoss(blank=blank_label, reduction='mean', zero_infinity=True)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
